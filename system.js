fb2202431330d3a18a3f0d958511d38634ed1139
mashu-png <85191016+mashu-png@users.noreply.github.com>
2022-02-09 02:04:31 +0900

    Update system.js

--- system.js
+++ system.js
@@ -2,3 +2,13 @@ let Ranking_Element = document.getElementById('Ranking');
 let date = new Date();
 let Month = date.getMonth()+1;
 Ranking_Element.innerHTML =Month + "月度1位";
+
+let header_Element = document.getElementById('DG_TOP');
+header_Element.addEventListener('click',function(){
+    location.href = 'http://www.genky.co.jp/';
+});
+
+let Tweet_btn_Element = document.getElementById('tweet');
+Tweet_btn_Element.addEventListener('click',function(){
+    location.href = 'https://twitter.com/ore30749492';
+});

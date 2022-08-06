860838ccfb1d5ebe59965cedbc4e902bd16175b7
mashu-png <85191016+mashu-png@users.noreply.github.com>
2022-08-06 19:54:20 +0900

    Create System.js

---
+++ Cast/Sho/System.js
@@ -0,0 +1,20 @@
+let Ranking_Element = document.getElementById('Ranking');
+let date = new Date();
+let Month = date.getMonth();
+if(Month === 0){
+    Ranking_Element.innerHTML ="12月度2位";
+    console.log(Month);
+}else{
+    Ranking_Element.innerHTML = Month + "月度2位";
+    console.log(Month);
+}
+
+let header_Element = document.getElementById('DG_TOP');
+header_Element.addEventListener('click',function(){
+    location.href = '../../index.html';
+});
+
+let Tweet_btn_Element = document.getElementById('tweet');
+Tweet_btn_Element.addEventListener('click',function(){
+    location.href = 'https://twitter.com/ore30749492';
+});

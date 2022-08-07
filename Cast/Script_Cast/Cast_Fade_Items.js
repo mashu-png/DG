a6bed01414ec6991a3e9eee8c6b880c7808c5015
mashu-png <85191016+mashu-png@users.noreply.github.com>
2022-08-08 00:55:29 +0900

    Update Cast_Fade_Items.js

--- Cast/Script_Cast/Cast_Fade_Items.js
+++ Cast/Script_Cast/Cast_Fade_Items.js
@@ -5,14 +5,14 @@
         setTimeout(Sho,4500);
     }
     function Riku(){
-        $('.Cast').hide().fadeIn(1500);
-        $('.Cast_Text_Logo').hide().fadeIn(1500);
-        $('.Cast_Name').hide().fadeIn(1500);
+        $('.Cast').hide().fadeIn(2000);
+        $('.Cast_Text_Logo').hide().fadeIn(2000);
+        $('.Cast_Name').hide().fadeIn(2000);
     }
     function Sho(){
-        $('.Cast1').hide().fadeIn(2000);
-        $('.Cast_Text_Logo1').hide().fadeIn(2000);
-        $('.Cast_Name1').hide().fadeIn(2000);
+        $('.Cast1').hide().fadeIn(2500);
+        $('.Cast_Text_Logo1').hide().fadeIn(2500);
+        $('.Cast_Name1').hide().fadeIn(2500);
     }
     main();
 }())

104c8cb992e1ad2b4362f145ae28514e6c4c2b3f
mashu-png <85191016+mashu-png@users.noreply.github.com>
2022-08-08 00:58:20 +0900

    Update Cast_Fade_Items.js

--- Cast/Script_Cast/Cast_Fade_Items.js
+++ Cast/Script_Cast/Cast_Fade_Items.js
@@ -1,18 +1,15 @@
 (function(){
 
-    function main(){
-        setTimeout(Riku,4000);
-        setTimeout(Sho,5000);
-    }
     function Riku(){
         $('.Cast').hide().fadeIn(2000);
         $('.Cast_Text_Logo').hide().fadeIn(2000);
         $('.Cast_Name').hide().fadeIn(2000);
     }
+    setTimeout(Riku,4000);
     function Sho(){
         $('.Cast1').hide().fadeIn(2500);
         $('.Cast_Text_Logo1').hide().fadeIn(2500);
         $('.Cast_Name1').hide().fadeIn(2500);
     }
-    main();
+    setTimeout(Sho,5000);
 }())
